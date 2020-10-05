b3ca5894dc8e3a19171bb7e54e5252dacfb1c17b
lwalther <lwalther@novareto.de>
2020-10-05 21:17:29 +0200

    Preparing release 1.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,8 +2,8 @@ Changelog
 =========
 
 
-1.0a1 (unreleased)
-------------------
+1.0 (2020-10-05)
+----------------
 
 - Initial release.
   [lwalther]
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='nva.webcode',
-    version='1.0a1',
+    version='1.0',
     description="Webcode-Behavior fuer Plone 5",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
